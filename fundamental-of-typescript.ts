b259b107ff76c11fadc2c902a003087d4e1e3eaf
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-10-31 11:06:36 +0600

    first commit

---
+++ fundamental-of-typescript.ts
@@ -0,0 +1,24 @@
+
+// JS Type in TS
+
+// Number
+// String
+// Boolean
+// null
+// undefined
+// Object
+// Symbol
+
+
+// TS Own Type
+
+// Interface
+// Void
+// Array
+// Tuple
+// Enum
+// Union
+// Intersection
+// Generic
+
+
