9a6c112abf5fb58870be750408aa6888140f083b
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-11-03 12:16:41 +0600

    update the configuration .tsc

---
+++ module_1/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// JavaScript
+// let Learn = "Learn TypeScript";
+// console.log(Learn);
+// TypeScript
+let Learn = "Learn TypeScript";
+console.log(Learn);
---
+++ module_1/src/index.ts
@@ -0,0 +1,11 @@
+// JavaScript
+
+// let Learn = "Learn TypeScript";
+// console.log(Learn);
+
+// TypeScript
+
+let Learn : string = "Learn TypeScript";
+console.log(Learn);
+
+
