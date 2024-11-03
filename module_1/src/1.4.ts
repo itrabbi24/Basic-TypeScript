a66a68a668286fc2ef6a70a247c3fe340eb329c3
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-11-03 17:20:39 +0600

    update some basic things

---
+++ module_1/src/1.4.ts
@@ -0,0 +1,33 @@
+// Basic Data Type
+
+// String
+let firstName : string = "John";
+
+// Number
+let age : number = 30;
+
+// Boolean
+let hasHobbies : boolean = true;
+
+// Undefined
+let lastName : undefined = undefined;
+
+// Null
+let job : null = null;
+
+// Array
+let hobbies : string[] = ["Sports", "Cooking"];
+
+// Number Array
+let mixed : any[] = ["John", 30, true];
+
+// Tuple
+let address : [string, number] = ["Superstreet", 99];
+
+// Enum
+enum Color {
+    Red = 1,
+    Green = 2,
+    Blue = 4
+}
+
