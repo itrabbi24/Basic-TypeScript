e18022048036cd8cc184958c8fb4a99631b0c4a6
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-11-03 18:34:45 +0600

    Type alias in typescript

---
+++ module_1/src/1.9..ts
@@ -0,0 +1,34 @@
+// Type alias in typescript
+
+type Student = {
+    name: string;
+    age: number;
+    gender: string;
+    contactNo?: string;
+    address: string;
+}
+
+const student1: Student = {
+    name: "John",
+    age: 30,
+    gender: "Male",
+    contactNo: "1234567890",
+    address: "123 Main Street"
+}
+
+const student2: Student = {
+    name: "John",
+    age: 30,
+    gender: "Male",
+    contactNo: "1234567890",
+    address: "123 Main Street"
+}
+
+
+const student3 : Student = {
+    name: "John",
+    age: 30,
+    gender: "Male",
+    contactNo: "1234567890",
+    address: "123 Main Street"
+}
