0d9bd6587ae4c905a246290cc739ad052a2430d8
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-11-04 17:20:15 +0600

    Interface, type vs interface

---
+++ module_2/2.2.ts
@@ -0,0 +1,22 @@
+// Interface, type vs interface
+
+// Interface
+interface Person {
+    name: string;
+    age: number;
+}    
+
+// Type
+type Person2 = {
+    name: string;
+    age: number;
+}
+
+
+
+//
+
+const user : Person = {
+    name: "John",
+    age: 30
+}
