ca8350ce39730e971cb5ea6bd65aaa1710d2f79a
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-11-05 12:30:33 +0600

    Interface, type vs interface

--- module_2/2.2.ts
+++ module_2/2.2.ts
@@ -33,3 +33,23 @@ const user2 : Person3 = {
     age: 30,
     gender: "Male"
 }
+
+
+
+type Roll = number[];
+
+interface Roll2{
+    [index: number]: number
+}
+
+const roleNumber1: Roll2 = [1,2,3];
+
+
+
+type Add1 = (num1: number, num2: number) => number;
+
+interface Add2{
+    (num1: number, num2: number): number
+}
+
+const add : Add2 = (num1, num2) => num1+ num2;
