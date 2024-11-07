6c2731bf85311351ef06d610afdb4c4b606c315e
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-11-07 12:03:11 +0600

    Polymorphism

--- module_3/3.8.ts
+++ module_3/3.8.ts
@@ -47,6 +47,62 @@ getSleepingHour(developer);
 
 
 
+class Shape{
+    getArea(): number{
+        return 0;
+    }
+}
+
+
+// Pi * radius * radius
+class Circle extends Shape{
+    radius : number;
+
+    constructor(radius: number){
+        super();
+        this.radius = radius;
+    }
+
+    getArea(): number{
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+
+// height * weight
+class Rectangle extends Shape{
+   height : number;
+   width : number;
+
+    constructor(height: number, width: number){
+        super();
+        this.height = height;
+        this.width = width;
+    }
+
+    getArea(): number{
+        return this.height * this.width;
+    }
+}
+
+
+const getShapeArea = (param: Shape)=>{
+    console.log(param.getArea());
+}
+
+getShapeArea(new Circle(10));
+getShapeArea(new Rectangle(10, 20));
+
+
+const shape1 = new Shape();
+const shape2 = new Circle(10);
+const shape3 = new Rectangle(10, 20);
+
+getShapeArea(shape1);
+getShapeArea(shape2);
+getShapeArea(shape3);
+
+
 
 
 
