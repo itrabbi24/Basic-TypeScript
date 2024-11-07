76a1b545db8fe397fa0407f9fc97ebcbf74166af
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-11-07 11:11:42 +0600

    // public     // private     // protected

---
+++ module_3/3.5.ts
@@ -0,0 +1,71 @@
+// Access modifiers
+
+{
+
+    // public
+    // private
+    // protected
+    // readonly
+    // static
+    // abstract
+    // readonly
+
+
+    class BackAccount{
+        public readonly id: number;
+        public name: string;
+        protected _balance: number;
+
+        constructor(id: number, name: string, balance: number){
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+
+        addDeposit(amount: number){
+            this._balance += amount;
+        }
+
+        getBalance(){
+            return this._balance
+        }
+
+    }
+
+
+    class StudentAccount extends BackAccount{
+        constructor(id: number, name: string, balance: number){
+            super(id, name, balance);
+        }
+    }
+
+
+const poorAccount =  new BackAccount(1, "John", 1000);
+
+poorAccount.addDeposit(1000);
+
+const balance = poorAccount.getBalance();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
